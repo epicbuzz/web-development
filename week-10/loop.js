fba5a142906b67eeccb03f8c7d6be957779e5467
Naveed Ahmad <naveedahmada036@gmail.com>
2024-09-07 15:27:24 +0500

    week 10

---
+++ week-10/loop.js
@@ -0,0 +1,44 @@
+/*const callMe = (i) => {
+  if( i == 100)
+    return i;
+
+  console.log("hello", i)
+  i+=1;
+
+  callMe(i)
+}
+
+
+callMe(0);
+*/
+
+/*
+[
+callMe, 0, ?
+callMe, 1, ?
+...
+callMe, 99, 99
+
+callMe, 100, 100
+]
+*/
+
+/*
+const fact = (num) => {
+   if(num == 0 || num == 1)
+     return 1;
+
+   console.log(`${num} * ${num-1}`);
+
+   return num * fact(num-1);
+}
+
+const result = fact(5);
+console.log(`result is  ${ result}`);
+
+r = 1;
+for(i=1; i<=5; i++){
+  r = r * i;
+}
+console.log(`result is  ${ r}`);
+*/
