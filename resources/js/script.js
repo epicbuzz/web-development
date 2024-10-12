c2c3992efb27dbd54e5e6c15926382c7c1f4b0c0
Naveed Ahmad <naveedahmada036@gmail.com>
2024-10-12 17:06:54 +0500

    Book shop

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,5 @@
 let classList = document.getElementById("classList");
-const weekCount = 12;
+const weekCount = 13;
 
 classList.classList.add("d-flex" ,"align-items-center", "flex-wrap" ,"gap-3" ,"my-5")
 
